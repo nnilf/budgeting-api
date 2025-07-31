97475044ce81ac0c93ffd2cb05a1203698827cf6
Flinn Mort <flinnmort13@outlook.com>
2025-07-31 21:41:28 +0100

    Add transaction model

--- main.py
+++ main.py
@@ -1,14 +1,35 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Body, Query
 from models import Account
+from enum import Enum
+from pydantic import BaseModel
+from typing import Annotated
 
 app = FastAPI()
 
+class Item(BaseModel):
+    name: str
+    description: Annotated[str | None, Query(max_length=50)] = None
+    price: float
+    tax: float | None = None
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+
+class Transaction(BaseModel):
+    SenderId : int
+    RecipientId : int
+    Amount : float
+    Category : Annotated[str, Body(max_length=50)] = 'Transfer'
+    Reference : Annotated[str | None, Body(max_length=50)] = None
 
 
-@app.put("/accounts/{account_id}")
-def update_item(account_id: int, account: Account):
-    return {"item_name": account.id, "account_id": account_id}
+@app.get("/items/")
+async def read_items(q: Annotated[str | None, Query(max_length=50)] = None):
+    results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
+    if q:
+        results.update({"q": q})
+    return results
+
+
+@app.get("/test/")
+async def get_test(test: str, q: str | None = None):
+    dictionary = {"query" : q, "test" : test}
+    return dictionary
